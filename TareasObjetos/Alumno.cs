73eab1fb370cdbb85c787f359a5b9b649c46e354
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-31 10:15:13 +0100

    ejer1

---
+++ TareasObjetos/Alumno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareasObjetos
+{
+    class Alumno
+    {
+        private string nombre;
+        private int edad;
+
+        public Alumno(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine("El alumno se llama {0} y tiene {1} años", this.nombre, this.edad);
+        }
+        public void SaberMayorEdad()
+        {
+            if (this.edad >= 18)
+            {
+                Console.WriteLine("El alumno es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("El alumno es menor de edad");
+            }
+        }
+    }
+}
---
+++ TareasObjetos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TareasObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno A1 = new Alumno("Gerardo Aguilar Gómez", 36);
+            A1.MostrarInfo();
+            A1.SaberMayorEdad();
+        }
+    }
+}
