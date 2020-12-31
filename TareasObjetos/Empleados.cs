cfc8b3d7022e9afbb1332dcdf49d456b8959bf95
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-31 10:21:25 +0100

    ejer2

---
+++ TareasObjetos/Empleados.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareasObjetos
+{
+    class Empleados
+    {
+        private string nombre;
+        private int sueldo;
+
+        public Empleados(string nombre, int sueldo)
+        {
+            this.nombre = nombre;
+            this.sueldo = sueldo;
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine("El nombre del empleado es {0} y su sueldo es de {1} €", this.nombre, this.sueldo);
+        }
+        public void Impuestos()
+        {
+            if (this.sueldo >= 3000)
+            {
+                Console.WriteLine("El empleado debe pagar impuestos");
+            }
+            else
+            {
+                Console.WriteLine("El empleado no debe pagar impuestos");
+            }
+        }
+    }
+}
--- TareasObjetos/Program.cs
+++ TareasObjetos/Program.cs
@@ -6,9 +6,15 @@ namespace TareasObjetos
     {
         static void Main(string[] args)
         {
+            /*
             Alumno A1 = new Alumno("Gerardo Aguilar Gómez", 36);
             A1.MostrarInfo();
             A1.SaberMayorEdad();
+            */
+
+            Empleados E1 = new Empleados("Anacleto Perez Sanchez", 3001);
+            E1.MostrarInformacion();
+            E1.Impuestos();
         }
     }
 }
