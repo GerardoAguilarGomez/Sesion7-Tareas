b7fd5fb092bf5d85506153731abc422b8d1efb85
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-31 10:27:46 +0100

    ejer3

--- TareasObjetos/Program.cs
+++ TareasObjetos/Program.cs
@@ -12,9 +12,17 @@ namespace TareasObjetos
             A1.SaberMayorEdad();
             */
 
+            /*
             Empleados E1 = new Empleados("Anacleto Perez Sanchez", 3001);
             E1.MostrarInformacion();
             E1.Impuestos();
+            */
+
+            Operaciones O1 = new Operaciones(54, 23);
+            O1.Suma();
+            O1.Resta();
+            O1.Multi();
+            O1.Divi();
         }
     }
 }
